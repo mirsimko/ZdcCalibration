1d4210df803bba6712a1d445daeb5950d6392baa
Miroslav Simko <mirsimko@gmail.com>
2018-03-12 15:51:34 -0400

    macro to set links

---
+++ setLinks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+fromDir=/direct/star+u/msimko/ZDC/ZDC_Calibration/ZdcCalibration
+
+ln -s "$fromDir"/html_maker.cpp   
+ln -s "$fromDir"/loadZDCtree.C      
+ln -s "$fromDir"/run.sh         
+ln -s "$fromDir"/zdcTree.C
+ln -s "$fromDir"/moveToWww.sh
+ln -s "$fromDir"/run.cxx              
+ln -s "$fromDir"/zdcTree.h
